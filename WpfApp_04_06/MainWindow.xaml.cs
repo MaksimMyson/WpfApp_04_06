f73639133d07df251a3477ae307d070a0c4e1706
Mayson Kotlyar Maksim <maksimmyson33@gmail.com>
2024-06-07 18:40:15 +0300

    was done

--- WpfApp_04_06/MainWindow.xaml.cs
+++ WpfApp_04_06/MainWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Text;
+﻿using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Text;
+using System.Timers;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -16,9 +19,41 @@ namespace WpfApp_04_06
     /// </summary>
     public partial class MainWindow : Window
     {
+        private System.Timers.Timer updateTimer;  // Явно вказуємо System.Timers.Timer
+        public ObservableCollection<Process> Processes { get; set; }
+
         public MainWindow()
         {
             InitializeComponent();
+            Processes = new ObservableCollection<Process>();
+            ProcessListView.ItemsSource = Processes;
+            updateTimer = new System.Timers.Timer(1000);  // Явно вказуємо System.Timers.Timer
+            updateTimer.Elapsed += UpdateProcesses;
+            updateTimer.Start();
+        }
+
+        private void UpdateProcesses(object sender, ElapsedEventArgs e)
+        {
+            Dispatcher.Invoke(() =>
+            {
+                Processes.Clear();
+                foreach (var process in Process.GetProcesses())
+                {
+                    Processes.Add(process);
+                }
+            });
+        }
+
+        private void UpdateButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (int.TryParse(IntervalTextBox.Text, out int interval))
+            {
+                updateTimer.Interval = interval;
+            }
+            else
+            {
+                MessageBox.Show("Please enter a valid number.");
+            }
         }
     }
 }
