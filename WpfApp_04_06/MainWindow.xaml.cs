cd9f432a9611988f433b24083956fe37f0931167
Mayson Kotlyar Maksim <maksimmyson33@gmail.com>
2024-06-07 18:53:15 +0300

    was done ex 1

--- WpfApp_04_06/MainWindow.xaml.cs
+++ WpfApp_04_06/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.Diagnostics;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -11,14 +12,44 @@ using System.Windows.Shapes;
 
 namespace WpfApp_04_06
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private async void LaunchProcess_Click(object sender, RoutedEventArgs e)
+        {
+            string processPath = ProcessPathTextBox.Text;
+            if (!string.IsNullOrEmpty(processPath))
+            {
+                try
+                {
+                    Process process = new Process();
+                    process.StartInfo.FileName = processPath;
+                    process.EnableRaisingEvents = true;
+
+                    process.Start();
+
+                    StatusTextBlock.Text = "Process is running...";
+
+                    // Asynchronously wait for the process to exit
+                    await Task.Run(() => process.WaitForExit());
+
+                    // After the process exits, get the exit code
+                    int exitCode = process.ExitCode;
+                    StatusTextBlock.Text = $"Process exited with code: {exitCode}";
+                }
+                catch (Exception ex)
+                {
+                    StatusTextBlock.Text = $"Error launching process: {ex.Message}";
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please enter the path to the application.");
+            }
         }
     }
 }
