302334f1d3202970ba046eb8490716dcc9f496ef
Mayson Kotlyar Maksim <maksimmyson33@gmail.com>
2024-06-07 19:07:26 +0300

    was done ex 3

--- WpfApp_04_06/MainWindow.xaml.cs
+++ WpfApp_04_06/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.Diagnostics;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -11,14 +12,40 @@ using System.Windows.Shapes;
 
 namespace WpfApp_04_06
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void ExecuteButton_Click(object sender, RoutedEventArgs e)
+        {
+            string num1 = Number1TextBox.Text;
+            string num2 = Number2TextBox.Text;
+            string operation = OperationTextBox.Text;
+
+            // Створюємо аргументи для передачі дочірньому процесу
+            string arguments = $"{num1} {num2} {operation}";
+
+            // Шлях до вашого дочірнього процесу
+            string processPath = "ChildProcess.exe";
+
+            // Запускаємо дочірній процес
+            ProcessStartInfo startInfo = new ProcessStartInfo();
+            startInfo.FileName = processPath;
+            startInfo.Arguments = arguments;
+            startInfo.RedirectStandardOutput = true;
+            startInfo.UseShellExecute = false;
+
+            using (Process process = Process.Start(startInfo))
+            {
+                // Отримуємо результати з дочірнього процесу
+                string result = process.StandardOutput.ReadToEnd();
+
+                // Відображаємо результат у текстовому полі
+                ResultTextBox.Text = result;
+            }
         }
     }
 }
